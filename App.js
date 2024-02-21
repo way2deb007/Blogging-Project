9e0df587f26e12a8a01bc2ab07f4dca736070af0
Debdulal Das <98931862+way2deb007@users.noreply.github.com>
2024-02-21 18:59:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Login from '.  /src/components/account/Login.jsx';
+
+function App() {
+  return (
+    <div style={{marginTop: 64}}>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
